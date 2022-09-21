fac77680b5056a6082df129199ad947d3ddcc8f6
Max Bruckner <max@maxbruckner.de>
2022-09-21 21:51:09 +0200

    Add async-time-mock-async-std

---
+++ async-time-mock-async-std/src/lib.rs
@@ -0,0 +1,42 @@
+use async_std::task::sleep;
+use std::time::Duration;
+
+#[derive(Clone)]
+pub enum MockableClock {
+	Real,
+	#[cfg(test)]
+	Mock(std::sync::Arc<async_time_mock_core::TimerRegistry>),
+}
+
+pub enum TimeHandlerGuard {
+	Real,
+	#[cfg(test)]
+	Mock(async_time_mock_core::TimeHandlerGuard),
+}
+
+#[cfg(test)]
+impl From<async_time_mock_core::TimeHandlerGuard> for TimeHandlerGuard {
+	fn from(guard: async_time_mock_core::TimeHandlerGuard) -> Self {
+		Self::Mock(guard)
+	}
+}
+
+impl MockableClock {
+	#[cfg(test)]
+	pub fn mock() -> (Self, std::sync::Arc<async_time_mock_core::TimerRegistry>) {
+		let timer_registry = std::sync::Arc::new(async_time_mock_core::TimerRegistry::default());
+		(Self::Mock(timer_registry.clone()), timer_registry)
+	}
+
+	pub async fn sleep(&self, duration: Duration) -> TimeHandlerGuard {
+		use MockableClock::*;
+		match self {
+			Real => {
+				sleep(duration).await;
+				TimeHandlerGuard::Real
+			}
+			#[cfg(test)]
+			Mock(registry) => registry.sleep(duration).await.into(),
+		}
+	}
+}
